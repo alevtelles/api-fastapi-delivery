31e7be8209436b40170032e375f23d5b4d4b3c3a
Alex Valente <alevtelles@gmail.com>
2025-07-14 09:54:08 -0300

    add criptografia nas autenticacao

--- auth_routes.py
+++ auth_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from models import Usuario
 from dependencies import pegar_sessao
+from main import bcrypt_context
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -19,7 +20,8 @@ async def criar_conta(email: str, nome: str, senha: str, session=Depends(pegar_s
     if usuario:
         return {"mensagem": "Usuário já cadastrado"}
     else:
-        novo_usuario = Usuario(nome, email, senha)
+        senha_criptografada = bcrypt_context.hash(senha)
+        novo_usuario = Usuario(nome, email, senha_criptografada)
         session.add(novo_usuario)
         session.commit()
         return {"mensagem": "Usuário cadastrado com sucesso!"}
